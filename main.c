1855d201990ff75025f94f8a249ba0556d803fc9
oc-loop-to <mohamex.mounib@gmail.com>
2025-05-21 05:03:56 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
-enum day{samedi=1,dimanche=2,lundi=3,maredi=4,mercredi=5,jeudi=6,vendredi=7};
+#include <string.h>
+typedef struct{
+char name[20];
+int age;
+}human;
+
+
+
 int main()
-{enum day today;
-printf("samedi=1,dimanche=2,lundi=3,maredi=4,mercredi=5,jeudi=6,vendredi=7");
-printf("\nwhat day is today:");
-scanf("%d",&today);
+{human a[10];
+    for(int i=0;i<2;i++){
+        printf("the name of human[%d]:",i+1);
+        fgets(a[i].name,20,stdin);
+
+        a[i].name[strlen(a[i].name)-1]='\0';
+        printf("the age of the human[%d]",i+1);
+        scanf("%d",&a[i].age);
+        getchar();
+    }
+    for(int i=0;i<2;i++){
+        printf("\n %s is %d years old",a[i].name,a[i].age);
 
 
-
-if(today==7 || today==1){
-    printf("it is the weekend");
-
-}else{
-
-printf("it is work day");
-
-
-}
-
+    }
     return 0;
 }
